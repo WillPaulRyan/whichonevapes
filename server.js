938195778a69ac3af8c05993a030cb43f2c9cb1e
Will Ryan <willpaulryan@gmail.com>
2020-01-21 19:45:28 +0100

    Heroke setup

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 
@@ -26,6 +27,16 @@ mongoose
 
 // Use routes
 app.use("/api/celebs", require("./routes/api/celebs"));
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 const PORT = process.env.PORT || 1337;
 
